406a678d84998b348a6a424c925c92403bb6f941
kai <jairvianen@icloud.com>
2026-02-06 08:10:14 +0100

    Initialize Discord bot with ping command

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ]
+});
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} is now running!`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
